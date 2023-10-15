f9ffb7837cf4920aad9f152f61a6ee5f77447b02
mlgzilla <rmlgzilla@gmail.com>
2023-10-15 18:35:47 +0300

    safasfa

--- app/src/main/java/com/example/laba2/MainActivity.java
+++ app/src/main/java/com/example/laba2/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.laba2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     ArrayList<String> myStringArray;
 
     @Override
@@ -23,10 +26,24 @@ public class MainActivity extends AppCompatActivity {
         Button buttonLang = findViewById(R.id.buttonLang);
         Button buttonAdd = findViewById(R.id.buttonAdd);
         Button buttonRm = findViewById(R.id.buttonRm);
+        EditText editText = findViewById(R.id.editText);
 
-        myStringArray = new ArrayList<String>();
-        ArrayAdapter<String> TextAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, myStringArray);
+        myStringArray = new ArrayList<>();
+        ArrayList<String> selectedElements = new ArrayList<>();
+        ArrayAdapter TextAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, myStringArray);
         textList.setAdapter(TextAdapter);
+
+        textList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                String element = (String) TextAdapter.getItem(position);
+                if (textList.isItemChecked(position))
+                    selectedElements.add(element);
+                else
+                    selectedElements.remove(element);
+            }
+        });
 
         buttonLang.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,22 +56,28 @@ public class MainActivity extends AppCompatActivity {
                     buttonAdd.setText("Add");
                     buttonRm.setText("Remove");
                 }
-                view.requestLayout();
+                TextAdapter.notifyDataSetChanged();
             }
         });
         buttonAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                myStringArray.add("element " + myStringArray.size());
-                view.requestLayout();
+                String elementName = editText.getText().toString();
+                TextAdapter.add(elementName);
+                TextAdapter.notifyDataSetChanged();
             }
         });
         buttonRm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (myStringArray.size()!=0) {
-                    myStringArray.remove(myStringArray.size()-1);
-                    view.requestLayout();
+                    textList.clearChoices();
+                    for(int i=0; i < selectedElements.size();i++)
+                    {
+                        TextAdapter.remove(selectedElements.get(i));
+                    }
+                    selectedElements.clear();
+                    TextAdapter.notifyDataSetChanged();
                 }
             }
         });
